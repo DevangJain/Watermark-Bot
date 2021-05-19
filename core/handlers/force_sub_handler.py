d2b127544368da261bd8cd4c82e98d023e0cff62
DevangJain <70439930+DevangJain@users.noreply.github.com>
2021-05-19 13:01:55 +0530

    Update force_sub_handler.py

--- core/handlers/force_sub_handler.py
+++ core/handlers/force_sub_handler.py
@@ -1,28 +1,20 @@
-# (c) @AbirHasan2005
-
-import asyncio
 from configs import Config
-from pyrogram.errors import FloodWait, UserNotParticipant
+from pyrogram.errors import UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-async def handle_force_subscribe(bot, cmd):
-    invite_link = None
-    try:
-        invite_link = await bot.create_chat_invite_link(int(Config.UPDATES_CHANNEL))
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        return
+async def handle_force_sub(bot, cmd):
+    invite_link = await bot.create_chat_invite_link(int(Config.UPDATES_CHANNEL))
     try:
         user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), cmd.from_user.id)
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                text="You Are Banned So  Contact my Owners  [【◉ Owners ◉】](https://t.me/Animesh_941/53).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
-            return
+            return 400
     except UserNotParticipant:
         await bot.send_message(
             chat_id=cmd.from_user.id,
@@ -30,21 +22,21 @@ async def handle_force_subscribe(bot, cmd):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
+                        InlineKeyboardButton("【😼 JOIN TO USE ME😼】", url=invite_link.invite_link)
                     ],
                     [
-                        InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshmeh")
+                        InlineKeyboardButton("【😼 REFRESH😼】", callback_data="refreshmeh")
                     ]
                 ]
             ),
             parse_mode="markdown"
         )
-        return
+        return 400
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+            text="Something went Wrong. Contact my Owners  [【◉ Team Librarian ◉】](https://t.me/Team_Librarian).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
-        return
+        return 400
